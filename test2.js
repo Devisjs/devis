16f207dbc05dbb08789d9e9df218b9e3b1816be9
ismailrei <ismailrei@gmail.com>
2016-08-11 12:07:38 +0100

    c

--- test2.js
+++ test2.js
@@ -1,21 +1,22 @@
-var devis=require("./devis")
+var devis = require("./devis")
 
-.client({
-  host: '127.0.0.1',
-  port: 3000,
-  timeout:400 
-})
-devis.act({
-    action: 'ping',
-    cmd:'play'
-  }, function(err, result) {
-    if(err) console.log(err);
-      console.log(result);
+  .client({
+    host: '127.0.0.1',
+    port: 3000,
+    timeout: 400
+  })
+devis.act({ action: 'ping', cmd: 'play' }, function (err, result) {
+
+  console.log(result);
 });
-devis.act({
-    action: 'ping',
-    cmd:'game'
-  }, function(err, result) {
-    if(err) console.log(err);
-      console.log(result);
+  devis.act({ action: 'ping', cmd: 'game' }, function (err2, result2) {
+  
+  
+  console.log(result2);
 });
+  devis.act({ action: 'drive', cmd: 'car' }, function (err2, result2) {
+  
+  
+  console.log(result2);
+});
+  
