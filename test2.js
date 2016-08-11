040557af0357e4c2eee045bf237ae36116784cf4
ismailrei <ismailrei@gmail.com>
2016-08-11 18:15:10 +0100

    commit

--- test2.js
+++ test2.js
@@ -5,9 +5,19 @@ var devis = require("./devis")
     port: 3000,
     timeout: 400
   }).setName("test2");
+  function test(id)
+  {
+    console.log(id);
+  }
 devis.act({ action: 'ping', cmd: 'play' }, function (err, result) {
 
   console.log(result);
+   devis.act({ action: 'ping', cmd: 'game' }, function (err2, result2) {
+  
+  
+  console.log(result2);
+});
+  
 });
   devis.act({ action: 'ping', cmd: 'game' }, function (err2, result2) {
   
@@ -19,4 +29,3 @@ devis.act({ action: 'ping', cmd: 'play' }, function (err, result) {
   
   console.log(result2);
 });
-  
