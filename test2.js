3c223b57f9b7e65aafc47a472bfffdb614976208
ismailrei <ismailrei@gmail.com>
2016-08-12 14:01:29 +0100

    commit

--- test2.js
+++ test2.js
@@ -1,29 +1,32 @@
 var devis = require("./devis")
+  devis.setName("test2")
   .client({
     host: '127.0.0.1',
-    port: 3000,
-    timeout: 400
-  }).setName("test2");
+    id:2,
+    port: 3030
+  })
+    .use("./test")
   function test(id)
   {
     console.log(id);
   }
-devis.act({ action: 'ping', cmd: 'play' }, function (err, result) {
+devis.act({action: 'ping', cmd: 'play' }, function (err, result) {
 
   console.log(result);
-   devis.act({ action: 'ping', cmd: 'game' }, function (err2, result2) {
+   devis.act({action: 'ping', cmd: 'game' }, function (err2, result2) {
   
   
   console.log(result2);
 });
   
 });
-  devis.act({ action: 'ping', cmd: 'game' }, function (err2, result2) {
+
+  devis.act({ clientId:2,action: 'test1', cmd: 'car' }, function (err2, result2) {
   
   
   console.log(result2);
 });
-  devis.act({ action: 'drive', cmd: 'car' }, function (err2, result2) {
+  devis.act({action: 'drive', cmd: 'car' }, function (err2, result2) {
   
   
   console.log(result2);
