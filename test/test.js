95de90a607280d3702b7b35d6bbcc83d8bf433a6
ismailrei <ismailrei@gmail.com>
2016-08-14 19:30:01 +0100

    c

---
+++ test/test.js
@@ -0,0 +1,23 @@
+var devis=require("../devis");
+
+devis.add({
+  action: 'game',
+  cmd:'play'
+}, function(args, done) {
+  
+  done(null, { result: 'play' });
+});
+devis.add({
+  action: 'game',
+  cmd:'pause'
+}, function(args, done) {
+    
+  done(null, { result: 'pause' });
+});
+devis.add({
+  action: 'gamer',
+  cmd:'play'
+}, function(args, done) {
+   
+  done(null, { result: 'gamer play' });
+})
