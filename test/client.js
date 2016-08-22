6127d53ff143483b4786cc042f904116a54bc018
ismailrei <ismailrei@gmail.com>
2016-08-22 11:24:00 +0100

    catch error when id is not unique or empty

--- test/client.js
+++ test/client.js
@@ -3,23 +3,33 @@ var devis=require("../devis")
   host:'127.0.0.1',
   port:3030,
   id:1
+})
+.client({
+  host:'127.0.0.1',
+  port:3030,
+  id:2
 }).setName('test2');
 
 devis.act({ clientId:1,action: 'game', cmd: 'play' }, function (err, result) {
 
   console.log(result);
-   
+
 });
 
-devis.act({clientId:1,action: 'game', cmd: 'pause' }, function (err2, result2) {
+devis.act({clientId:1,action: 'game', cmd: 'pause'}, function (err2, result2) {
 
 
   console.log(result2);
 });
 
 
-  devis.act({ clientId:1,action: 'gamer', cmd: 'play' }, function (err2, result2) {
+  devis.act({ clientId:2,action: 'gamer', cmd: 'play' }, function (err2, result2) {
 
 
   console.log(result2);
+});
+devis.act({ clientId:2,action: 'gamer', cmd: 'play' }, function (err2, result2) {
+
+
+console.log(result2);
 });
