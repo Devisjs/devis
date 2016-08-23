3749338a083c9b2bc7905dfc920f447a50a7bc71
ismailrei <ismailrei@gmail.com>
2016-08-23 16:43:23 +0100

    add all handles local and distant on a log file

--- test/client.js
+++ test/client.js
@@ -2,34 +2,32 @@ var devis=require("../devis")
 .client({
   host:'127.0.0.1',
   port:3030,
-  id:1
+  id:2
 })
 .client({
   host:'127.0.0.1',
   port:3030,
-  id:2
+  id:1
 }).setName('test2');
-
+devis.add({test:"test",action:"done"},(args,done)=>{
+  done(null,"test done");
+})
 devis.act({ clientId:1,action: 'game', cmd: 'play' }, function (err, result) {
 
   console.log(result);
 
 });
 
-devis.act({clientId:1,action: 'game', cmd: 'pause'}, function (err2, result2) {
+devis.act({clientId:1,action: 'game', cmd: 'pause' }, function (err2, result2) {
 
 
   console.log(result2);
 });
 
 
-  devis.act({ clientId:2,action: 'gamer', cmd: 'play' }, function (err2, result2) {
+  devis.act({ clientId:1,action: 'gamer', cmd: 'play' }, function (err2, result2) {
 
 
   console.log(result2);
-});
-devis.act({ clientId:2,action: 'gamer', cmd: 'play' }, function (err2, result2) {
-
-
-console.log(result2);
-});
+})
+.log();
