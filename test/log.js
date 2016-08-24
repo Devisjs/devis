30bb872987c56c2b1c8fa14360acfc2e24a02310
Ismail Reida <ismailrei@gmail.com>
2016-08-24 16:11:00 +0100

    Update log.js

--- test/log.js
+++ test/log.js
@@ -1,5 +1,22 @@
-let devis=require("../devis");
-devis.use(process.argv[2]);
-devis.setName("devis");
+let devis = require("../devis");
 
+if (process.argv[2] == "connect") {
+    let i = 3;
+    let res = {};
+
+    while (process.argv[i]) {
+        if (process.argv[i] != ":" && process.argv[i] != ",") {
+            res[process.argv[i]] = process.argv[i + 2];
+            i += 3;
+        } else i++;
+    }
+
+    res['id'] = 1;
+
+    devis.client(res);
+
+
+} else devis.use(process.argv[2]);
+
+devis.setName("devis");
 devis.log();
