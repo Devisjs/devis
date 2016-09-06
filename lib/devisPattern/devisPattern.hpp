87451ff6a54f3ecf6a08399c09d2c6069e1c16fe
ismailrei <ismailrei@gmail.com>
2016-09-06 22:29:56 +0100

    fisex some bugs

--- lib/devisPattern/devisPattern.hpp
+++ lib/devisPattern/devisPattern.hpp
@@ -13,14 +13,14 @@
 using namespace std;
 using namespace v8;
 
-typedef map<string, int> TStrStrMap;
+typedef map<string, int, std::greater<string>> TStrStrMap;
 typedef pair<string,int> TStrStrPair;
 typedef TStrStrMap::iterator it_type;
 
 namespace devisPattern {
-    
+
     class devisPattern : public node::ObjectWrap {
-        
+
     public:
         static void Init(v8::Local<v8::Object> exports);
         static void NewInstance(const v8::FunctionCallbackInfo<v8::Value>& args);
@@ -28,16 +28,16 @@ namespace devisPattern {
     private:
         explicit devisPattern();
         ~devisPattern();
-        
+
         static void New(const v8::FunctionCallbackInfo<v8::Value>& args);
         static void list(const v8::FunctionCallbackInfo<v8::Value>& args);
         static void find(const v8::FunctionCallbackInfo<v8::Value>& args);
         static void add(const v8::FunctionCallbackInfo<v8::Value>& args);
         static v8::Persistent<v8::Function> constructor;
-        static TStrStrMap tMap;
+         TStrStrMap tMap;
 
     };
-    
+
 }
 
 #endif
