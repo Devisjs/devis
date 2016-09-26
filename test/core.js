7306d6a4716b5996c2643af1a4b478225dc3062e
ismailrei <ismailrei@gmail.com>
2016-09-26 01:17:18 +0100

    change some details

--- test/core.js
+++ test/core.js
@@ -1,21 +1,21 @@
 'use strict';
 var devis=require("../devis");
 
-devis.add({
+devis.define({
   action: 'game',
   cmd:'play'
 }, function(args, done) {
 
   done({ result: 'play' });
 });
-devis.add({
+devis.define({
   action: 'game',
   cmd:'pause'
 }, function(args, done) {
 
   done({ result: 'pause' });
 });
-devis.add({
+devis.define({
   action: 'gamer',
   cmd:'play'
 }, function(args, done) {
