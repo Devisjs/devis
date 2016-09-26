1ca85df8ef2e67e62d9953655b4ba0a8b896a67f
ismailrei <ismailrei@gmail.com>
2016-09-26 01:22:36 +0100

    fix test file

--- test/test.js
+++ test/test.js
@@ -1,20 +1,20 @@
 'use strict';
 let devis=require("../devis");
 devis.use("./core");
-devis.act({action: 'game', cmd: 'play' }, function (result) {
+devis.fetch({action: 'game', cmd: 'play' }, function (result) {
 
 	console.log(result);
 
     });
 
-devis.act({action: 'game', cmd: 'pause' }, function (result2) {
+devis.fetch({action: 'game', cmd: 'pause' }, function (result2) {
 
 
 	console.log(result2);
     });
 
 
-devis.act({action: 'gamer', cmd: 'play' }, function (result2) {
+devis.fetch({action: 'gamer', cmd: 'play' }, function (result2) {
 
 
 	console.log(result2);
