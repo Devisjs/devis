e8ff22afa260f0f4d958c2874f50c4c3bbbbe0c8
ismailrei <ismailrei@gmail.com>
2016-10-21 23:22:28 +0100

    add error first

--- test/client.js
+++ test/client.js
@@ -1,27 +1,27 @@
-const devis=require("../devis")
-.client({
-  host:'127.0.0.1',
-  port:3030,
-  id:1
-}).setName('test2');
-devis.add({test:"test",action:"done"},(args,done)=>{
-  done("test done");
+const devis = require("../devis")
+  .client({
+    host: '127.0.0.1',
+    port: 3030,
+    id: 1
+  }).setName('test2');
+devis.add({ test: "test", action: "done" }, (args, done) => {
+  done(null, "test done");
 })
-devis.act({ clientId:1,action: 'game', cmd: 'play' }, function (result) {
-
+devis.act({ clientId: 1, action: 'game', cmd: 'play' }, function (err, result) {
+  if (err) throw (err);
   console.log(result);
 
 });
 
-devis.act({clientId:1,action: 'game', cmd: 'pause' }, function (result2) {
-
+devis.act({ clientId: 1, action: 'game', cmd: 'pause' }, function (err, result2) {
+  if (err) throw (err);
 
   console.log(result2);
 });
 
 
-  devis.act({ clientId:1,action: 'gamer', cmd: 'play' }, function (result2) {
+devis.act({ clientId: 1, action: 'gamer', cmd: 'play' }, function (err, result2) {
 
-
+  if (err) throw (err);
   console.log(result2);
 })
