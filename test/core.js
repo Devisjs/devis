aaa7fab230ec9757522595770338de06a42d8b25
ismailrei <ismailrei@gmail.com>
2016-10-21 23:37:57 +0100

    update readme and test

--- test/core.js
+++ test/core.js
@@ -4,17 +4,18 @@ var devis=require("../devis");
 devis.add({
   action: 'game',
   cmd:'play'
-}, function(args, done) {
+}, function(args,done) {
 
-  done(null,{ result: 'play' });
+  done(null,args.name+" is playing game now");
 });
 devis.add({
   action: 'game',
   cmd:'pause'
 }, function(args, done) {
 
-  done(null,{ result: 'pause' });
+  done(null,args.name+" pause the game");
 });
+
 devis.add({
   action: 'gamer',
   cmd:'play'
