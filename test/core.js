60b1cc9fa3b93d93c1931fda028d0493485c23fe
ismailrei <ismailrei@gmail.com>
2016-11-11 00:57:36 +0000

    pack data sent to the client

--- test/core.js
+++ test/core.js
@@ -1,25 +1,27 @@
 'use strict';
-var devis=require("../devis");
+var devis = require("../devis");
 
 devis.add({
-  action: 'game',
-  cmd:'play'
-}, function(args,done) {
-
-  done(null,args.name+" is playing game now");
-});
-devis.add({
-  action: 'game',
-  cmd:'pause'
+    action: 'game',
+    cmd: 'play'
 }, function(args, done) {
 
-  done(null,args.name+" pause the game");
+    done(null, args.name + " is playing game now");
+});
+devis.add({
+    action: 'game',
+    cmd: 'pause'
+}, function(args, done) {
+
+    done(null, args.name + " pause the game");
 });
 
 devis.add({
-  action: 'gamer',
-  cmd:'play'
+    action: 'gamer',
+    cmd: 'play'
 }, function(args, done) {
 
-  done(null,{ result: args.name});
+    done(null, {
+        result: args.name
+    });
 })
